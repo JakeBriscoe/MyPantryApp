9d56a9e055d1c0292b5dfe70a8363f0297da7cf0
Tyler <matty040@student.otago.ac.nz>
2020-08-16 16:22:52 +1200

    Can now view items on home screen, BUT have to use hardware device as a bug occurs when using emulator

--- app/src/main/java/com/example/mypantryapp/domain/Product.java
+++ app/src/main/java/com/example/mypantryapp/domain/Product.java
@@ -1,9 +1,14 @@
 package com.example.mypantryapp.domain;
 
+import com.google.firebase.Timestamp;
+
+import java.util.Date;
+
 public class Product {
     private long productId;
     private float weight;
     private long barcodeNum;
+    private long shelfLife; // in days
     private String productName;
     private String description;
     private String brand;
@@ -16,6 +21,7 @@ public class Product {
         this.productId = productId;
         this.weight = weight;
         this.barcodeNum = barcodeNum;
+        this.shelfLife = 7;
         this.productName = productName;
         this.description = description;
         this.brand = brand;
@@ -23,85 +29,54 @@ public class Product {
         this.dietaryType = dietaryType;
         this.allergens = allergens;
         this.ingredients = ingredients;
+    }
+
+    public Product(long productId, float weight, long barcodeNum, long shelfLife, String productName, String description, String brand, String categoryName, String dietaryType, String allergens, String ingredients) {
+        this(productId, weight, barcodeNum, productName, description, brand, categoryName, dietaryType, allergens, ingredients);
+        this.shelfLife = shelfLife;
     }
 
     public long getProductId() {
         return productId;
     }
 
-    public void setProductId(long productId) {
-        this.productId = productId;
-    }
-
     public float getWeight() {
         return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
     }
 
     public long getBarcodeNum() {
         return barcodeNum;
     }
 
-    public void setBarcodeNum(long barcodeNum) {
-        this.barcodeNum = barcodeNum;
+    public long getShelfLife() {
+        return shelfLife;
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public String getCategoryName() {
         return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
     }
 
     public String getDietaryType() {
         return dietaryType;
     }
 
-    public void setDietaryType(String dietaryType) {
-        this.dietaryType = dietaryType;
-    }
-
     public String getAllergens() {
         return allergens;
     }
 
-    public void setAllergens(String allergens) {
-        this.allergens = allergens;
-    }
-
     public String getIngredients() {
         return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
     }
 }
