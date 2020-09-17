fb10c6d24cbd590e39bcb46bd3b4395616e76958
Regan James <jamre937@student.otago.ac.nz>
2020-09-18 11:15:11 +1200

    Toast to indicate item is added to user's pantry

--- app/src/main/java/com/example/mypantryapp/BottomSheetDialog.java
+++ app/src/main/java/com/example/mypantryapp/BottomSheetDialog.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +23,16 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        Button bottomModalConfirm = v.findViewById(R.id.bottomModalConfirm);
+        bottomModalConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), nameText + " added to pantry", Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        });
+
         return v;
     }
 
