15c1da1b6946894d3001fdad1f9bc830b55b26c8
Regan James <jamre937@student.otago.ac.nz>
2020-10-07 00:45:39 +1300

    Redirect to home when settings are saved, small design changes

--- app/src/main/java/com/example/mypantryapp/SettingsFragment.java
+++ app/src/main/java/com/example/mypantryapp/SettingsFragment.java
@@ -286,6 +286,7 @@ public class SettingsFragment extends Fragment {
                 @Override
                 public void onClick(View v) {
                     setItem(view);
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).addToBackStack("HomeFragment").commit();
                 }
             });
         }
